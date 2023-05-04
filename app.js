7130ea08e8f1f903f8e8464cb721efa41350561d
romjanali1 <mdromgana@gmail.com>
2023-05-04 16:37:52 +0600

    add-to-card

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
 document.getElementById('player-1').addEventListener('click', function(){
-    console.log('ok baby')
+    const player = document.getElementById('name-1');
+    const playerName = player.innerText;
+    
+    const playerAdd = document.getElementById('player-add');
+    const li = document.createElement('li');
+    li.innerText = playerName;
+
+    playerAdd.appendChild(li);
+
+})
+
+document.getElementById('player-2').addEventListener('click', function(){
+    const player = document.getElementById('name-2');
+    const playerName = player.innerText;
+    
+    const playerAdd = document.getElementById('player-add');
+    const li = document.createElement('li');
+    li.innerText = playerName;
+
+    playerAdd.appendChild(li);
+
 })
